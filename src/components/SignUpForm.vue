cec4a5106a0855c404e169bead45a75c608be39b
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-06-20 03:28:24 +0500

    added delete functions for removing skills, submit and form validation features

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,10 +1,13 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
       <label>Email:</label>
       <input type="email" required v-model="email">
 
        <label>Password:</label>
       <input type="password" required v-model="password">
+      <div v-if="passwordError" class="error">
+          {{passwordError}}
+      </div>
 
       <label>Role: </label>
       <select v-model="role">
@@ -15,12 +18,16 @@
       <label>Skills:</label>
       <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
       <div v-for="skill in skills" :key="skill" class="pill">
-              {{skill}}
+          <span  @click="deleteSkill(skill)">{{skill}}</span>
         </div>
 
       <div class="terms">
           <input type="checkbox" v-model="terms" required>
           <label>accept terms and conditions</label>
+      </div>
+
+      <div class="submit">
+          <button>Create an Account</button>
       </div>
 
       <!-- <div>
@@ -53,7 +60,8 @@ export default {
             terms: false,
             // names: []
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
         }
     },
 
@@ -65,7 +73,18 @@ export default {
                 }
                 this.tempSkill = ''
             }
+        },
 
+        deleteSkill(skill){
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
+        },
+
+        handleSubmit(){
+            //validate password
+            this.passwordError = this.password.length > 5 ? 
+            '' : 'Password must be 6 chars long'
         }
     }
 }
@@ -107,5 +126,38 @@ input[type="checkbox"]{
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+
+.pill{
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    counter-reset: #777;
+    cursor: pointer;
+}
+
+button{
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.submit{
+    text-align: center;
+}
+
+.error{
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
